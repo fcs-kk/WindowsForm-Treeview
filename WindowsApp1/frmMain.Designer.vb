02174bd36eaabcb90b96fc6c16fe327043f6699e
Y.Chisaki <yutaka.chisaki@fcs-kk.co.jp>
2022-02-16 15:40:43 +0900

    WindowsForm の TreeViewサンプル

---
+++ WindowsApp1/frmMain.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.treeView = New System.Windows.Forms.TreeView()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblInputTitle = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'treeView
+        '
+        Me.treeView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.treeView.CheckBoxes = True
+        Me.treeView.Location = New System.Drawing.Point(12, 149)
+        Me.treeView.Name = "treeView"
+        Me.treeView.Size = New System.Drawing.Size(482, 321)
+        Me.treeView.TabIndex = 0
+        '
+        'txtInput
+        '
+        Me.txtInput.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtInput.Location = New System.Drawing.Point(12, 28)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(482, 84)
+        Me.txtInput.TabIndex = 1
+        Me.txtInput.Text = "A,A,A,1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A,A,A,2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A,B,C1,1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A,B,C2,1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A,B,C3,1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "A,B,B,1"
+        '
+        'lblInputTitle
+        '
+        Me.lblInputTitle.AutoSize = True
+        Me.lblInputTitle.Location = New System.Drawing.Point(12, 9)
+        Me.lblInputTitle.Name = "lblInputTitle"
+        Me.lblInputTitle.Size = New System.Drawing.Size(57, 12)
+        Me.lblInputTitle.TabIndex = 2
+        Me.lblInputTitle.Text = "入力データ"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 134)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "出力ツリービュー"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(372, 118)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(121, 27)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "変換"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(506, 482)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblInputTitle)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.treeView)
+        Me.Name = "frmMain"
+        Me.Text = "ツリービューサンプル"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents treeView As TreeView
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblInputTitle As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnConvert As Button
+End Class
---
+++ WindowsApp1/frmMain.vb
@@ -0,0 +1,128 @@
+﻿Public Class frmMain
+
+    Private carrierRootNode As ClsCarrierNode = New ClsCarrierNode()   '' ルートキャリアノード
+
+
+    ''' <summary>
+    ''' フォームロード
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '' 無処理
+    End Sub
+
+    ''' <summary>
+    ''' キャリアノードの作成
+    ''' </summary>
+    ''' <param name="srcText">ソーステキスト</param>
+    ''' <returns>キャリアノード</returns>
+    Private Function createCarrierData(srcText As String) As ClsCarrierNode
+        Dim retVal = New ClsCarrierNode '' 返り値
+
+        '' 1行ずつ分解
+        For Each txt As String In txtInput.Lines
+            Dim currentNode = retVal
+
+            '' カンマで分解
+            Dim names = txt.Split(",")
+            For Each name As String In names
+                'カレントノードが同名の場合、スキップ
+                If currentNode.Name.Equals(name) Then
+                    Continue For
+                End If
+
+                'サブノードを探索
+                Dim subNode = currentNode.Nodes.Find(Function(s) s.Name = name)
+                If (subNode Is Nothing) Then
+                    ' 同名のサブノードがない場合、追加する
+                    Dim newNode = New ClsCarrierNode(name)
+                    currentNode.Nodes.Add(newNode)
+
+                    ' 新しいノードをカレントノードとする
+                    currentNode = newNode
+                Else
+                    ' 同名のサブノードが見つかったので、サブノードをカレントノードとする
+                    currentNode = subNode
+                End If
+            Next
+        Next
+
+        '' キャリアノードを返す
+        Return retVal
+    End Function
+
+    ''' <summary>
+    ''' ツリービューを構築
+    ''' </summary>
+    ''' <param name="carrierNode">キャリアノードデータ</param>
+    Private Sub buildTreeView(carrierNode As ClsCarrierNode)
+        ' ツリーの初期化
+        treeView.Nodes.Clear()
+
+        If Not carrierNode Is Nothing Then
+            '初期ノードの作成
+            Dim treeNode = New TreeNode
+
+            'ノードデータの読み込み
+            loadNodeData(carrierNode, treeNode)
+
+            'ノードの追加
+            For Each currentNode As TreeNode In treeNode.Nodes
+                treeView.Nodes.Add(currentNode)
+            Next
+        End If
+
+        ' ツリーの全てを展開
+        treeView.ExpandAll()
+    End Sub
+
+    ''' <summary>
+    ''' ノードにデータを読み込む
+    ''' </summary>
+    ''' <param name="srcNode">元ノードデータ</param>
+    ''' <param name="destNode">読み込み先のツリーノード</param>
+    Private Sub loadNodeData(srcNode As ClsCarrierNode, destNode As TreeNode)
+
+        '元ノードデータをループ
+        For Each currentNode As ClsCarrierNode In srcNode.Nodes
+
+            '' ノードデータからツリーノードを作成
+            Dim treeNode = destNode.Nodes.Add(currentNode.Key, currentNode.Name)
+            treeNode.Checked = currentNode.Checked
+
+            '' 子ノードがある場合、再帰的に繰り返す
+            If 0 < srcNode.Nodes.Count Then
+                loadNodeData(currentNode, treeNode)
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' ノードチェックイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub treeView_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles treeView.AfterCheck
+
+        'ノードデータから該当データを検索
+        Dim target = carrierRootNode.ChildNodes.Find(Function(s) s.Key = e.Node.Name)
+        If (target IsNot Nothing) Then
+            '' ツリービューに合わせて、ノードデータを更新する
+            target.Checked = e.Node.Checked
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 変換ボタン押下イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        'キャリアノード作成
+        carrierRootNode = createCarrierData(txtInput.Text)
+
+        'ノードデータからツリービューを構築
+        buildTreeView(carrierRootNode)
+    End Sub
+End Class
