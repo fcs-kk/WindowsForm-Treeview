aead988a433027103cbc5935b61a964b441489f3
Y.Chisaki <yutaka.chisaki@fcs-kk.co.jp>
2022-02-16 16:31:03 +0900

    TreeNodeだけで実現したバージョン

--- WindowsApp1/frmMain.vb
+++ WindowsApp1/frmMain.vb
@@ -1,6 +1,7 @@
 ﻿Public Class frmMain
 
-    Private carrierRootNode As ClsCarrierNode = New ClsCarrierNode()   '' ルートキャリアノード
+    Private rootNode = New TreeNode()               '' ルートノード
+    Private nodeList = New List(Of TreeNode)        '' ノードリスト 
 
 
     ''' <summary>
@@ -15,10 +16,11 @@
     ''' <summary>
     ''' キャリアノードの作成
     ''' </summary>
-    ''' <param name="srcText">ソーステキスト</param>
     ''' <returns>キャリアノード</returns>
-    Private Function createCarrierData(srcText As String) As ClsCarrierNode
-        Dim retVal = New ClsCarrierNode '' 返り値
+    Private Function createCarrierData() As TreeNode
+        Dim retVal = New TreeNode  '' 返り値
+
+        Dim srcText = txtInput.Text
 
         '' 1行ずつ分解
         For Each txt As String In txtInput.Lines
@@ -26,24 +28,38 @@
 
             '' カンマで分解
             Dim names = txt.Split(",")
+
             For Each name As String In names
                 'カレントノードが同名の場合、スキップ
-                If currentNode.Name.Equals(name) Then
+                If currentNode.Text.Equals(name) Then
                     Continue For
                 End If
 
                 'サブノードを探索
-                Dim subNode = currentNode.Nodes.Find(Function(s) s.Name = name)
-                If (subNode Is Nothing) Then
-                    ' 同名のサブノードがない場合、追加する
-                    Dim newNode = New ClsCarrierNode(name)
+                Dim findNode As TreeNode = Nothing
+                For Each subNode As TreeNode In currentNode.Nodes
+                    If subNode.Text.Equals(name) Then
+                        findNode = subNode
+                        Exit For
+                    End If
+                Next
+
+                ' 同名のサブノードがない場合、追加する
+                If (findNode Is Nothing) Then
+                    Dim newNode = New TreeNode(name)
+                    newNode.Name = Guid.NewGuid.ToString    'ユニークキーを生成
+
+                    ' ノードリストに追加する
+                    nodeList.Add(newNode)
+
+                    ' カレントノードにサブノードを追加する
                     currentNode.Nodes.Add(newNode)
 
-                    ' 新しいノードをカレントノードとする
+                    ' サブノードをカレントノードとする
                     currentNode = newNode
                 Else
                     ' 同名のサブノードが見つかったので、サブノードをカレントノードとする
-                    currentNode = subNode
+                    currentNode = findNode
                 End If
             Next
         Next
@@ -56,46 +72,19 @@
     ''' ツリービューを構築
     ''' </summary>
     ''' <param name="carrierNode">キャリアノードデータ</param>
-    Private Sub buildTreeView(carrierNode As ClsCarrierNode)
+    Private Sub buildTreeView(carrierNode As TreeNode)
         ' ツリーの初期化
         treeView.Nodes.Clear()
 
         If Not carrierNode Is Nothing Then
-            '初期ノードの作成
-            Dim treeNode = New TreeNode
-
-            'キャリアノードからツリーノードへ変換
-            convertNodeData(carrierNode, treeNode)
-
             'ノードの追加
-            For Each currentNode As TreeNode In treeNode.Nodes
+            For Each currentNode As TreeNode In carrierNode.Nodes
                 treeView.Nodes.Add(currentNode)
             Next
         End If
 
         ' ツリーの全てを展開
         treeView.ExpandAll()
-    End Sub
-
-    ''' <summary>
-    ''' キャリアノードからツリーノードへ変換
-    ''' </summary>
-    ''' <param name="srcCarrierNode">変換元キャリアノード</param>
-    ''' <param name="destTreeNode">変換先ツリーノード</param>
-    Private Sub convertNodeData(srcCarrierNode As ClsCarrierNode, destTreeNode As TreeNode)
-
-        'キャリアノードをループ
-        For Each currentNode As ClsCarrierNode In srcCarrierNode.Nodes
-
-            '' ノードデータからツリーノードを作成
-            Dim treeNode = destTreeNode.Nodes.Add(currentNode.Key, currentNode.Name)
-            treeNode.Checked = currentNode.Checked
-
-            '' 子ノードがある場合、再帰的に繰り返す
-            If 0 < srcCarrierNode.Nodes.Count Then
-                convertNodeData(currentNode, treeNode)
-            End If
-        Next
     End Sub
 
     ''' <summary>
@@ -106,10 +95,10 @@
     Private Sub treeView_AfterCheck(sender As Object, e As TreeViewEventArgs) Handles treeView.AfterCheck
 
         'ノードデータから該当データを検索
-        Dim target = carrierRootNode.ChildNodes.Find(Function(s) s.Key = e.Node.Name)
+        Dim target = rootNode.Nodes.Find(e.Node.Name, True)
         If (target IsNot Nothing) Then
             '' ツリービューに合わせて、ノードデータを更新する
-            target.Checked = e.Node.Checked
+            Console.WriteLine(e.Node.Name + ":" + e.Node.Checked.ToString)
         End If
     End Sub
 
@@ -120,9 +109,9 @@
     ''' <param name="e"></param>
     Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
         'キャリアノード作成
-        carrierRootNode = createCarrierData(txtInput.Text)
+        rootNode = createCarrierData()
 
         'ノードデータからツリービューを構築
-        buildTreeView(carrierRootNode)
+        buildTreeView(rootNode)
     End Sub
 End Class
